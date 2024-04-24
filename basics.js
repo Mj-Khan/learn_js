ced0ddd1446af425ff81e53405c137aa51d0a736
Mj-Khan <mjkhan7124@gmail.com>
2024-04-24 18:13:02 +0530

    initial commit

---
+++ basics.js
@@ -0,0 +1,48 @@
+// document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+console.log("---------------")
+
+//1. Create a variable, myAge, and set its value to your age
+//2. Log the myAge variable to the console
+let myAge = 26
+console.log("my age is " + myAge)
+console.log("---------------")
+
+
+//Arithmetic Operations
+let firstBatch = 5
+let secondBatch = 7
+let total = firstBatch + secondBatch
+console.log(total) 
+console.log("---------------")
+
+/*-----------------------*/
+// 1. Create two variables, [myAge] and [humanDogRatio]
+// 2. Multiply the two together and store the result in [myDogAge]
+// 3. Log [myDogAge] to the console
+
+//As i already have a variable called [myAge] at line 9 will use that
+// Reassigning new value to [myAge] variable
+myAge = 28
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+console.log("---------------")
+
+// Create a variable, Initialize it as 50. Increase it to 100
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonosPoints = 50
+console.log(bonosPoints)
+bonosPoints = bonosPoints + 50
+console.log(bonosPoints)
+bonosPoints = bonosPoints - 75
+console.log(bonosPoints)
+bonosPoints = bonosPoints + 45
+console.log(bonosPoints)
+console.log("---------------")
